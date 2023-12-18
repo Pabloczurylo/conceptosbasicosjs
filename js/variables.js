18604f044abe8761e536069c51c287028f52bf34
Pablo Czurylo <pabloczurylo10@gmail.com>
2023-12-18 20:37:28 -0300

    agregamos mensaje por consola, documento html y ventana emergente

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+// Esto es un comentario de una sola linea
+/*
+ Comentarios de varias lineas 
+*/
+// mostrar comentarios desde el archivo de js
+console.log("Mensaje por consola");
+// mostrar un comentario en el documento html
+document.write('<p class="text-center text-primary">Primer mensaje en el documento HTML</p>');
+// ventanas emergentes 
+
+//alert('Ejemplo de alerta');
