4e1babc6993c74dffa28b227068c03e598ea46f1
Pablo Czurylo <pabloczurylo10@gmail.com>
2024-01-12 17:28:43 -0300

    agregamos un ejemplo de condicionales dobles

---
+++ js/condicionales.js
@@ -0,0 +1,16 @@
+const edad = parseInt(prompt('Ingrese su edad'));
+
+//If( condicion logica){
+   // Todo el codigo que quiero ejecutar si se cumple la condición 
+// }
+
+if( edad >= 18 && edad<=71){
+    alert('Puede tener un permiso para conducir');
+
+} else{
+    if( edad < 18){
+        alert('Sos menor de edad, no puedes tener un permiso para conducir')
+    }else{
+        alert('No puede manejar');
+    }
+}
